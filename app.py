38cc218e9c380c8182c4d4988bf75d991b782cd0
aguilar036 <cristianantonio411@gmail.com>
2024-09-04 23:30:07 -0600

    Inicial

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect
+from pymysql import MySQLError
+
+from sql import get_conn, create_tables
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return render_template("index.html")
+
+@app.route("/show")
+def show():
+    conn = get_conn()
+    cur = conn.cursor()
+    contacts = []
+
+    try:
+        cur.execute("SELECT * FROM agenda;")
+        contacts = cur.fetchall()
+    except MySQLError as e:
+        print(e)
+    finally:
+        cur.close()
+        conn.close()
+
+    return render_template("out.html", contacts=contacts)
+
+@app.route("/add", methods=["POST"])
+def form():
+    if request.method != "POST":
+        return 404
+
+    firstName = request.form["name"]
+    lastName = request.form["lastname"]
+
+    connection = get_conn()
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute("INSERT INTO agenda (name, lastname) VALUES (%s, %s)", (firstName, lastName))
+        connection.commit()
+    except MySQLError as e:
+        print(e)
+        return "Error:(",400
+    finally:
+        cursor.close()
+        connection.close()
+
+    return redirect("show")
+if __name__ == "__main__":
+    create_tables()
+    app.run()
---
+++ sql.py
@@ -0,0 +1,25 @@
+from linecache import cache
+
+import pymysql
+
+def get_conn():
+    return pymysql.connect(
+        host='localhost',
+        port=3306,
+        user='root',
+        password='',
+        db='test',
+    )
+
+def create_tables():
+    conn = get_conn()
+    cur = conn.cursor()
+
+    try:
+        cur.execute("CREATE TABLE IF NOT EXISTS agenda(name TEXT, lastname TEXT);")
+        conn.commit()
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+        conn.close()
